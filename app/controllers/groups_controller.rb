d7e038d4066f5377a4c2d60ad422ffe27ace82ab
EC2 Default User <104773179+kazuwaki@users.noreply.github.com>
2022-06-22 07:29:29 +0000

    [finish]groupメンバー

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -13,9 +13,16 @@ class GroupsController < ApplicationController
     @group = Group.new
   end
 
+  def join
+    @group = Group.find(params[:group_id])
+    @group.users << current_user
+    redirect_to groups_path
+  end
+
   def create
     @group = Group.new(group_params)
     @group.owner_id = current_user.id
+    @group.users << current_user
     if @group.save
       redirect_to groups_path
     else
@@ -30,11 +37,17 @@ class GroupsController < ApplicationController
   def update
     @group = Group.find(params[:id])
     @group.owner_id = current_user.id
-    if @group.update
+    if @group.update(group_params)
       redirect_to group_path(@group)
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.users.delete(current_user)
+    redirect_to groups_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/search', to: 'searches#search'
 
-  resources :groups, only: [:index, :show, :edit, :new, :update, :create]
+  resources :groups do
+    get "join" => "groups#join"
+  end
 end
